b85515b69bb4d0805f81eb78414cc4007e463f6a
Леонид Федотов <ileonidze@inbox.ru>
2014-02-19 17:01:05 +0400

    Fixes

--- js/flappybird.js
+++ js/flappybird.js
@@ -17,10 +17,10 @@ VK.api('isAppUser',{},function(data){
 VK.api('users.get',{},function(data){
 	if(data.response){
 		console.log(data.response);
-		console.log(data.response.first_name);
+		console.log(data.response.['first_name']);
 		var USERNAME = data.response['first_name'];
 		var USERSURNAME = data.response.last_name;
-		var USERID = data.response.uid;
+		var USERID = data.response['uid'];
 	} else {
 		var USERNAME = 'Anonimous';
 		var USERSURNAME = 'User';
